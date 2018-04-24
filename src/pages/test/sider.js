3a010de7c2a19870453fd472df61bb6035f97bfc
sunxinfei <sunxinfei147@gmail.com>
2018-04-24 21:29:23 +0800

    修复修改组名的bug，添加后台领域搜索的ajax后台

--- src/pages/test/sider.js
+++ src/pages/test/sider.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
-import { Layout, Cascader, Input, Button, Icon, Checkbox, List } from 'antd';
+import { Layout, Cascader, Input, Icon, Checkbox, List, Button } from 'antd';
 const { Sider } = Layout;
 const Search = Input.Search;
 const CheckboxGroup = Checkbox.Group;
@@ -16,20 +16,8 @@ class MySider extends Component {
 		super(props);
 		this.state = {
 			showSearch: false,
-			data233: [
-				{
-					title: 'Title 1'
-				},
-				{
-					title: 'Title 2'
-				},
-				{
-					title: 'Title 3'
-				},
-				{
-					title: 'Title 4'
-				}
-			]
+			searchValue: '',
+			selectGroupArr: []
 		};
 	}
 	componentDidMount() {
@@ -45,8 +33,102 @@ class MySider extends Component {
 			}
 		});
 	}
-	cancleSave() {
-		alert(123);
+	onInputChange(e) {
+		let _serachText = e.target.value;
+		// console.log(_groupName);
+		this.state.searchValue = _serachText;
+	}
+	onInputSearch() {
+		console.log(this.state.searchValue, '搜索开始');
+	}
+	onCascaderChange(value) {
+		let cascaderValueArr = value;
+		if (cascaderValueArr.length === 1) {
+			return;
+		}
+		const ownModuleID = cascaderValueArr[1];
+		Ajax({
+			url: `/nccloud/platform/appregister/queryapplazy.do`,
+			data: {
+				own_module: ownModuleID
+			},
+			success: (res) => {
+				const { data, success } = res.data;
+				if (success && data && data.length > 0) {
+					console.log(data);
+					this.setState({ selectGroupArr: data });
+				}
+			}
+		});
+		console.log(value, '选择', this.state);
+	}
+	switchSearch() {
+		const { showSearch } = this.state;
+		this.setState({ showSearch: !showSearch });
+	}
+	getSearchDom() {
+		let itemDom;
+		if (this.state.showSearch) {
+			itemDom = (
+				<div className='sider-search'>
+					<Input
+						placeholder='请输入应用名称'
+						style={{ width: '213px' }}
+						onPressEnter={this.onInputSearch.bind(this)}
+						onChange={this.onInputChange.bind(this)}
+						addonAfter={
+							<Icon type='search' className='search-input-icon' onClick={this.onInputSearch.bind(this)} />
+						}
+					/>
+					<span className='switch-search-cancel' onClick={this.switchSearch.bind(this)}>
+						取消
+					</span>
+				</div>
+			);
+		} else {
+			itemDom = (
+				<div className='sider-search'>
+					<Cascader
+						className='search-cascader'
+						style={{ width: '213px' }}
+						options={this.state.domainArr}
+						onChange={(value) => {
+							this.onCascaderChange(value);
+						}}
+						placeholder='领域-模块'
+					/>
+					<span className='switch-search' onClick={this.switchSearch.bind(this)}>
+						<Icon type='search' />
+					</span>
+				</div>
+			);
+		}
+		return itemDom;
+	}
+	getResultDom() {
+        return this.state.selectGroupArr.map((item, index) => {
+            return (
+                <div className='result-group-list'>
+                    <h4 className='result-header'>
+                        <Checkbox>{item.label}</Checkbox>
+                    </h4>
+                    <div className='result-app-list'>
+                        {
+                            item.children.map((child, i) => {
+                                return (
+                                    <div className='app-col'>
+                                        <div className='list-item'>
+                                            <div className='list-item-content'>{child.label}</div>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
+
+                    </div>
+                </div>
+            )
+        })
 	}
 	render() {
 		const contentHeight = this.props.contentHeight;
@@ -59,88 +141,25 @@ class MySider extends Component {
 				style={{ height: contentHeight }}
 			>
 				<div className='sider-content'>
-					<div className='sider-search'>
-						{/* <Search
+					{/* <Search
 						placeholder='请输入应用名称'
 						onSearch={(value) => console.log(value)}
-						style={{ width: 200 }}
-                        enterButton
-                        disabled={true}
-					/> */}
+						style={{ width: 222 }}
+					/>
 						<Cascader
 							style={{ width: 222 }}
 							options={this.state.domainArr}
 							onChange={onChange}
 							placeholder='领域-模块'
 						/>
-                        <Icon type="search" className="switch-search"/>
+                         */}
+
+					{this.getSearchDom()}
+					<div className='add-item'>
+						<span />
+						<span className='add'>添加</span>
 					</div>
-					<div className="add-item">
-                        <span></span>
-						<span className="add">添加</span>
-					</div>
-					<div className='sider-result'>
-						<div className='result-group-list'>
-                        <h4 className="result-header"><Checkbox>第一组</Checkbox></h4>				
-							<div className='result-app-list'>
-								<div className='app-col'>
-									<div className='list-item'>
-										<div className='list-item-content'>app1</div>
-									</div>
-								</div>
-								<div className='app-col'>
-									<div className='list-item'>
-										<div className='list-item-content'>app1</div>
-									</div>
-								</div>
-								<div className='app-col'>
-									<div className='list-item'>
-										<div className='list-item-content'>app1</div>
-									</div>
-								</div>
-							</div>
-						</div>
-                        <div className='result-group-list'>
-                        <h4 className="result-header"><Checkbox>第一组</Checkbox></h4>				
-							<div className='result-app-list'>
-								<div className='app-col'>
-									<div className='list-item'>
-										<div className='list-item-content'>app1</div>
-									</div>
-								</div>
-								<div className='app-col'>
-									<div className='list-item'>
-										<div className='list-item-content'>app1</div>
-									</div>
-								</div>
-								<div className='app-col'>
-									<div className='list-item'>
-										<div className='list-item-content'>app1</div>
-									</div>
-								</div>
-							</div>
-						</div><div className='result-group-list'>
-                        <h4 className="result-header"><Checkbox>第一组</Checkbox></h4>				
-							<div className='result-app-list'>
-								<div className='app-col'>
-									<div className='list-item'>
-										<div className='list-item-content'>app1</div>
-									</div>
-								</div>
-								<div className='app-col'>
-									<div className='list-item'>
-										<div className='list-item-content'>app1</div>
-									</div>
-								</div>
-								<div className='app-col'>
-									<div className='list-item'>
-										<div className='list-item-content'>app1</div>
-									</div>
-								</div>
-							</div>
-						</div>
-                        
-					</div>
+					<div className='sider-result'>{this.getResultDom()}</div>
 				</div>
 			</Sider>
 		);
