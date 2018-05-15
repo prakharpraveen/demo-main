16934ea6dbc2f301a083946ac6a66cdfc9953cd2
sunxinfei <sunxinfei147@gmail.com>
2018-05-15 15:51:28 +0800

    anchor.js

---
+++ src/workbench_front/pages/DesktopSetting/anchor.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
+import './index.less';
+import { connect } from 'react-redux';
+
+class AnchorLi extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+	targetAnchor = () => {
+		const targetID = this.props.id;
+		//id不能以数字开头
+		document.querySelector(`#a${targetID}`).scrollIntoView();
+	};
+	render() {
+		const { name } = this.props;
+		return <li onClick={this.targetAnchor}>{name}</li>;
+	}
+}
+
+class MyContentAnchor extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+	render() {
+		const { groups, contentHeight } = this.props;
+		return (
+			<ul className='content-anchor'>
+				{groups.map((g, i) => {
+					return <AnchorLi key={g.pk_app_group} id={g.pk_app_group} name={g.groupname} />;
+				})}
+			</ul>
+		);
+	}
+}
+
+export default connect(
+	(state) => ({
+		groups: state.templateDragData.groups
+	}),
+	{}
+)(MyContentAnchor);
