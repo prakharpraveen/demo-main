f839812cb3b9968612797400988afd3cb53bebf1
gaokung <gaokung@yonyou.com>
2018-05-16 21:27:39 +0800

    update

--- src/workbench_front/layout/SideDrawer.js
+++ src/workbench_front/layout/SideDrawer.js
@@ -11,23 +11,62 @@ class SideDrawer extends Component {
 	handleDrawerChange = (isOpen) => {
 		this.props.changeDrawer(isOpen);
 	};
+	handleExit = ()=>{
+		console.log("退出");
+	}
 	render() {
 		let { isOpen } = this.props;
 		return (
-			<Drawer className='nc-workbench-drawer' open={isOpen} onChange={this.handleDrawerChange}>
-				<div className='drawer-top'>
-					<div className='drawer-logo'>
-						<img src='http://www.qqzhi.com/uploadpic/2014-09-23/000247589.jpg' alt='logo' />
+			<div className='nc-workbench-drawer'>
+				<Drawer className='drawer-content' width={430} overlayColor={'none'} drawerStyle={{top:"48px","box-shadow": "3px 6px 8px 0px rgba(74,81,93,0.25)", 
+				"border-radius": "2px 3px 3px 0px" }} open={isOpen} onChange={this.handleDrawerChange}>
+					<div className='drawer-exit'>
+						<i className='iconfont icon-zhuxiao' onClick={this.handleExit}></i>
 					</div>
-				</div>
-				<div className='drawer-buttom'>
-					{/* <ul>
-                    <li>
-                        <Link to='/ar'>添加应用</Link>
-                    </li>
-                </ul> */}
-				</div>
-			</Drawer>
+					<div className='drawer-info'>
+						<div className='info'>
+							<div className='drawer-logo'>
+									<img src='http://www.qqzhi.com/uploadpic/2014-09-23/000247589.jpg' alt='logo' />
+							</div>
+							<span className='name'>
+								用户名称
+							</span>
+						</div>
+					</div>
+					<div className='drawer-setting'>
+						<div className='setting-content'>
+							<div className='setting-btn'>
+								<i className="iconfont icon-bianji"></i>
+								<span>个人设置</span>
+							</div>
+							<div className='setting-btn'>
+								<i className="iconfont icon-shezhi"></i>
+								<span>账户设置</span>
+							</div>
+							<div className='setting-btn'>
+								<i className="iconfont icon-shezhi"></i>
+								<span>个性化设置</span>
+							</div>
+						</div>
+					</div>
+					<div className='drawer-link'>
+						<ul className='link'>
+							<li>
+								<span>帮助</span>
+							</li>
+							<li>
+								<span>联系用友服务人员</span>
+							</li>
+							<li>
+								<span>云注册链接</span>
+							</li>
+							<li>
+								<span>日志</span>
+							</li>
+						</ul>
+					</div>
+				</Drawer>
+			</div>
 		);
 	}
 }
