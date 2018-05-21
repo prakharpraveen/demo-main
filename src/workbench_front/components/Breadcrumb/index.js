ab4f2a53fd758e3d6d5aa75b43424a9b410b6e7e
gaokung <gaokung@yonyou.com>
2018-05-21 21:17:38 +0800

    update

--- src/workbench_front/components/Breadcrumb/index.js
+++ src/workbench_front/components/Breadcrumb/index.js
@@ -1,14 +1,19 @@
 import React,{Component} from 'react';
 import { Breadcrumb as Bc } from 'antd';
+import { withRouter } from 'react-router';
 import './index.less';
 class Breadcrumb extends Component{
     constructor(props) {
         super(props);
         
     }
+    handleHomeClick=()=>{
+        this.props.history.push('/');
+    }
     render() {
         return (
             <div className='workbench-breadcrumb'>
+                <i className='iconfont icon-zhuye' onClick={this.handleHomeClick}></i>
                 <Bc separator={<i className='iconfont icon-mianbaoxie font-size-12'></i>}>
                     <Bc.Item><a href="">Application Center</a></Bc.Item>
                     <Bc.Item><a href="">Application List</a></Bc.Item>
@@ -18,4 +23,4 @@ class Breadcrumb extends Component{
         );
     }
 }
-export default Breadcrumb;
+export default withRouter(Breadcrumb);
