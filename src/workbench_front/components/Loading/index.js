6cdff50feaae3ea343fcc21e96df807644e7bf1f
glud <glud123@163.com>
2018-05-25 03:19:02 +0800

    update

--- src/workbench_front/components/Loading/index.js
+++ src/workbench_front/components/Loading/index.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { Spin } from 'antd';
 export default class Loading extends Component {
 	constructor(props) {
 		super(props);
 	}
 	render() {
-		return <div>loading...</div>;
+		return (
+			<div>
+				<Spin size="large" />
+			</div>
+		);
 	}
 }
