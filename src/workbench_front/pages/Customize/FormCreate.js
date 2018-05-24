c51fa00ad9e32b75a044d65d8591a6faf453c966
glud <glud123@163.com>
2018-05-25 03:04:20 +0800

    更新个性化设置页面

--- src/workbench_front/pages/Customize/FormCreate.js
+++ src/workbench_front/pages/Customize/FormCreate.js
@@ -1,8 +1,71 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
+import { Form, Row, Col } from 'antd';
+import { high } from 'nc-lightapp-front';
+import 'nc-lightapp-front/dist/platform/nc-lightapp-front/index.css';
+const { Refer } = high;
+const FormItem = Form.Item;
 class FormCreate extends Component {
-    constructor(props) {
-        super(props);
-        
-    }
-    
+	constructor(props) {
+		super(props);
+		console.log(props);
+	}
+	/**
+     * 创建表单项
+     */
+	createFormItem() {
+		const { getFieldDecorator } = this.props.form;
+		let children = this.props.formData.map((item, index) => {
+			let { refName, refCode, value } = item;
+			return (
+				<Col xs={24} md={12} lg={12} key={refCode}>
+					<FormItem className="form-item" label={refName}>
+						{getFieldDecorator(refCode, {
+							initialValue: {},
+							rules: [
+								{
+									required: false,
+									message: ''
+								},
+								{
+									type: 'object',
+									validator: null
+								}
+							]
+						})(<Refer {...item} />)}
+					</FormItem>
+				</Col>
+			);
+		});
+		return children;
+	}
+	/**
+     * 设置表单数据
+     * @param {Object} objValue 对象
+     */
+	getFormData = () => {
+		const { getFieldsValue } = this.props.form;
+		return getFieldsValue();
+	};
+	/**
+     * 设置表单数据
+     * @param {Object} objValue 对象
+     */
+	setFormData = (objValue) => {
+		const { setFieldsValue } = this.props.form;
+		setFieldsValue(objValue);
+	};
+	componentDidMount = () => {
+		this.props.getFormData(this.getFormData);
+		this.props.setFormData(this.setFormData);
+	};
+
+	render() {
+		return (
+			<Form layout="inline">
+				<Row>{this.createFormItem()}</Row>
+			</Form>
+		);
+	}
 }
+const FormContent = Form.create()(FormCreate);
+export default FormContent;
--- src/workbench_front/pages/Customize/index.js
+++ src/workbench_front/pages/Customize/index.js
@@ -19,37 +19,37 @@ class Customize extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			activeKey:'default',
-			listArray:[
+			activeKey: 'default',
+			listArray: [
 				{
-					name:'默认设置',
-					active:true,
-					code:'default'
+					name: '默认设置',
+					active: true,
+					code: 'default'
 				},
 				{
-					name:'审批语设置',
-					code:'approveLanguage'
+					name: '审批语设置',
+					code: 'approveLanguage'
 				},
 				{
-					name:'常用数据',
-					code:'3'
+					name: '常用数据',
+					code: '3'
 				},
 				{
-					name:'代理人设置',
-					code:'4'
+					name: '代理人设置',
+					code: '4'
 				},
 				{
-					name:'集团财务合并',
-					code:'5'
+					name: '集团财务合并',
+					code: '5'
 				},
 				{
-					name:'单体财务报表',
-					code:'6'
+					name: '单体财务报表',
+					code: '6'
 				},
 				{
-					name:'集团报表',
-					code:'7'
-				},
+					name: '集团报表',
+					code: '7'
+				}
 			]
 		};
 	}
@@ -57,49 +57,46 @@ class Customize extends Component {
 	 * 个性化页面页面
 	 * @param {String} key 菜单标识
 	 */
-	hanleMenuListClick = (key)=>{
+	hanleMenuListClick = (key) => {
 		let { listArray } = this.state;
 		// 为选中list 项添加 活跃标识
-		listArray = listArray.map((item,index)=>{
+		listArray = listArray.map((item, index) => {
 			delete item.active;
-			if(item.code === key){
+			if (item.code === key) {
 				item.active = true;
 			}
 			return item;
 		});
-		this.setState({listArray,activeKey:key});
+		this.setState({ listArray, activeKey: key });
 		switch (key) {
 			case 'value':
-				
 				break;
-		
+
 			default:
 				break;
 		}
-	}
+	};
 	/**
 	 * 
 	 */
-	loadCom = (key)=>{
+	loadCom = (key) => {
 		switch (key) {
 			case 'default':
-				return <DefaultSetting title={'默认设置'}/>;
+				return <DefaultSetting title={'默认设置'} />;
 			case 'approveLanguage':
-				return <ApproveLanguage title={'审批语设置'}/>;
+				return <ApproveLanguage title={'审批语设置'} />;
 			default:
 				break;
 		}
-	}
+	};
 	render() {
-		let {listArray} = this.state;
+		let { listArray } = this.state;
 		return (
 			<PageLayout>
 				<PageLayoutLeft>
-					<MenuList onClick ={this.hanleMenuListClick} listArray = {listArray}></MenuList>
+					<MenuList onClick={this.hanleMenuListClick} listArray={listArray} />
 				</PageLayoutLeft>
-				<PageLayoutRight>
-					{this.loadCom(this.state.activeKey)}
-				</PageLayoutRight>
+				<PageLayoutRight>{this.loadCom(this.state.activeKey)}</PageLayoutRight>
 			</PageLayout>
 		);
 	}
