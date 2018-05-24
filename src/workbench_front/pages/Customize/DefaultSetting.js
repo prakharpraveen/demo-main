515ff37ede06a73da09e0088686939d75d3b1c1c
gaokung <gaokung@yonyou.com>
2018-05-24 21:17:41 +0800

    update

--- src/workbench_front/pages/Customize/DefaultSetting.js
+++ src/workbench_front/pages/Customize/DefaultSetting.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react';
+import { Form, Row, Col, Input, Button, Icon } from 'antd';
 import ComLayout from './ComLayout';
-
+const FormItem = Form.Item;
 class DefaultSetting extends Component{
     constructor(props) {
         super(props);
@@ -10,7 +11,23 @@ class DefaultSetting extends Component{
             <ComLayout className='defaultSetting' title={this.props.title}>
                 <div className='default-title'>默认设置</div>
                 <div className='dafault-form'>
-                    123
+                    <Form
+                        className="ant-advanced-search-form"
+                        onSubmit={this.handleSearch}
+                    >
+                        <Row gutter={24}>{this.getFields()}</Row>
+                        <Row>
+                        <Col span={24} style={{ textAlign: 'right' }}>
+                            <Button type="primary" htmlType="submit">Search</Button>
+                            <Button style={{ marginLeft: 8 }} onClick={this.handleReset}>
+                            Clear
+                            </Button>
+                            <a style={{ marginLeft: 8, fontSize: 12 }} onClick={this.toggle}>
+                            Collapse <Icon type={this.state.expand ? 'up' : 'down'} />
+                            </a>
+                        </Col>
+                        </Row>
+                    </Form>
                 </div>
                 <div className='default-title'>默认语言格式</div>
                 <div className='dafault-form'>
