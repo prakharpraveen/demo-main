24c9d0c1b3d98f28dd400fd1ba2677cdf2dfce88
glud <glud123@163.com>
2018-05-27 22:18:24 +0800

    添加全新的创建树方法

---
+++ src/workbench_front/pages/MenuItem/TreeTable.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Table } from 'antd';
+class TreeTable extends Component {
+	constructor(props) {
+		super(props);
+		this.columns = [
+			{
+				title: '编码',
+				dataIndex: 'menuitemcode',
+				key: 'menuitemcode'
+			},
+			{
+				title: '名称',
+				dataIndex: 'menuitemname',
+				key: 'menuitemname'
+			},
+			{
+				title: '多语',
+				dataIndex: 'resid',
+				key: 'resid'
+			},
+			{
+				title: '操作',
+				dataIndex: 'action',
+				key: 'action',
+				render: () => {
+					return (
+						<div>
+							<span>111</span>
+							<span>222</span>
+							<span>333</span>
+						</div>
+					);
+				}
+			}
+		];
+	}
+	render() {
+		return (
+			<Table
+				rowKey={this.props.rowKey}
+				columns={this.columns}
+				rowSelection={this.props.rowSelection}
+				dataSource={this.props.treeTableData}
+			/>
+		);
+	}
+}
+export default TreeTable;
--- src/workbench_front/pages/MenuItem/index.js
+++ src/workbench_front/pages/MenuItem/index.js
@@ -1,29 +1,57 @@
-import React,{Component} from 'react';
-import { Button,Table,Switch,Icon,Popconfirm  } from 'antd';
-import {PageLayout} from 'Components/PageLayout';
+import React, { Component } from 'react';
+import { Button, Table, Switch, Icon, Popconfirm } from 'antd';
+import { PageLayout } from 'Components/PageLayout';
+import TreeTable from './TreeTable';
+import Ajax from 'Pub/js/ajax.js';
+import { createTree } from 'Pub/js/createTree.js';
+import TreeData from './data.json';
 import './index.less';
-class MenuItem extends Component{
-    constructor(props) {
-        super(props);
-        
-    }
-    render() {
-        return (
-            <PageLayout className="nc-workbench-menuitem">
-                <div className="nc-workbench-menuitem-card">
-                    <div className='menuitem-card-title'>
-                        菜单注册
-                        <Button type='primary'>保存</Button>
-                    </div>
-                    <div className='menuitem-card-form'>
-                        
-                    </div>
-                    <div className='menuitem-card-table'>
-                        
-                    </div>
-                </div>
-            </PageLayout>
-        );
-    }
+console.log(TreeData);
+
+class MenuItem extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			treeTableData: []
+		};
+	}
+	componentDidMount = () => {
+		let treeTableData = createTree(TreeData.data, 'menuitemcode', 'parentcode');
+		console.log(treeTableData);
+		this.setState({ treeTableData });
+		// Ajax({
+		// 	url: `/nccloud/platform/appregister/queryappmenus.do`,
+		// 	info: {
+		// 		name: '菜单注册',
+		// 		action: '应用树查询'
+		// 	},
+		// 	data: '',
+		// 	success: (res) => {
+		// 		let { success, data } = res.data;
+		// 		if (success && data) {
+		// 			let treeTableData = createTree(data);
+		// 			this.setState({ treeTableData });
+		// 		}
+		// 	}
+		// });
+	};
+
+	render() {
+		let { treeTableData } = this.state;
+		return (
+			<PageLayout className="nc-workbench-menuitem">
+				<div className="nc-workbench-menuitem-card">
+					<div className="menuitem-card-title">
+						菜单注册
+						<Button type="primary">保存</Button>
+					</div>
+					<div className="menuitem-card-form" />
+					<div className="menuitem-card-table">
+						<TreeTable rowKey={'menuitemcode'} treeTableData={treeTableData} />
+					</div>
+				</div>
+			</PageLayout>
+		);
+	}
 }
 export default MenuItem;
