e80548e276a31d0c7df96dec9842930af04c18ec
sunxinfei <sunxinfei147@gmail.com>
2018-06-04 13:42:18 +0800

    error

--- src/workbench_front/pages/ZoneSetting/rightSider.js
+++ src/workbench_front/pages/ZoneSetting/rightSider.js
@@ -8,8 +8,9 @@ import PropTypes from 'prop-types';
 import Ajax from 'Pub/js/ajax';
 import './index.less';
 import BasicProperty from './basicProperty';
-import { Tabs } from 'antd';
+import { Tabs , Input, Checkbox } from 'antd';
 const TabPane = Tabs.TabPane;
+import * as utilService from './utilService';
 import { updateSelectCard, updateAreaList } from 'Store/ZoneSetting/action';
 
 class MyRightSider extends Component {
@@ -19,6 +20,92 @@ class MyRightSider extends Component {
 	}
 
 	componentDidMount() {}
+
+	changeValue = (e, propertyKey) => {
+		let { selectCard } = this.props;
+		if (_.isEmpty(selectCard)) {
+			console.log('empty');
+			return;
+		}
+		console.log(e.target.value, propertyKey);
+		selectCard = { ...selectCard };
+		selectCard[propertyKey] = e.target.value;
+		this.props.updateSelectCard(selectCard);
+	};
+	updateCardInArea = (e, propertyKey) => {
+		let { areaList, selectCard } = this.props;
+		if (_.isEmpty(selectCard)) {
+			console.log('empty');
+			return;
+		}
+		let targetAreaIndex = 0;
+		let targetCardIndex = 0;
+		areaList = _.cloneDeep(areaList);
+		_.forEach(areaList, (a, i) => {
+			_.forEach(a.queryPropertyList, (q, index) => {
+				if (q.areaid === selectCard.areaid && q.pk_query_property === selectCard.pk_query_property) {
+					targetAreaIndex = i;
+					targetCardIndex = index;
+					return false;
+				}
+			});
+		});
+		areaList[targetAreaIndex].queryPropertyList[targetCardIndex][propertyKey] = selectCard[propertyKey];
+		this.props.updateAreaList(areaList);
+	};
+	changeCheckboxValue = (e, property) => {
+		let { selectCard } = this.props;
+		if (_.isEmpty(selectCard)) {
+			console.log('empty');
+			return;
+		}
+		selectCard = { ...selectCard };
+		let targetValue = -1;
+		if (e.target.checked) {
+			targetValue = '1';
+		} else {
+			targetValue = '0';
+		}
+		selectCard[property] = targetValue;
+		this.asyncUpdateSelectCard(selectCard).then(() => {
+			this.updateCardInArea(e, property);
+		});
+	};
+	onPressEnter = (e, property) => {
+		this[`${property}input`].blur();
+	};
+	getMyInput(placeholder, property) {
+		return (
+			<Input
+				placeholder={placeholder}
+				value={this.props.selectCard[property]}
+				onChange={(e) => {
+					this.changeValue(e, property);
+				}}
+				onBlur={(e) => {
+					this.updateCardInArea(e, property);
+				}}
+				ref={(input) => (this[`${property}input`] = input)}
+				onPressEnter={(e) => {
+					this.onPressEnter(e, property);
+				}}
+			/>
+		);
+	}
+	getMyCheckbox = (property) => {
+		return (
+			<Checkbox
+				checked={Boolean(Number(this.props.selectCard[property]))}
+				onChange={(e) => {
+					this.changeCheckboxValue(e, property);
+				}}
+			/>
+		);
+	};
+	async asyncUpdateSelectCard(selectCard) {
+		let user = await this.props.updateSelectCard(selectCard);
+		return user;
+	}
 
 	//
 	getMetaType = (selectCard) => {
@@ -40,14 +127,66 @@ class MyRightSider extends Component {
 	};
 
 	getDom1 = () => {
+		const {selectCard} = this.props;
 		return (
 			<Tabs defaultActiveKey='1'>
-				<TabPane tab='显示属性' key='1' />
+				<TabPane tab='显示属性' key='1'>
+					<ul className='basic-property'>
+						{/* <li>项目主键</li>
+				<li>{selectCard.metapath}</li> */}
+						<li>显示名称</li>
+						<li>{this.getMyInput('显示名称', 'label')}</li>
+						<li>编码</li>
+						<li>{selectCard.code}</li>
+						<li>元数据路径</li>
+						<li>{selectCard.metapath}</li>
+						<li>参照编码</li>
+						<li>{selectCard.refcode}</li>
+						<li>参照名称</li>
+						<li>{selectCard.refname}</li>
+						<li>显示顺序</li>
+						<li>{selectCard.position}</li>
+						<li>控件宽度</li>
+						<li>{this.getMyInput('控件宽度', 'width')}</li>
+						<li>录入长度</li>
+						<li>{this.getMyInput('录入长度', 'maxlength')}</li>
+						<li>下拉选项</li>
+						<li>待定</li>
+						<li>是否可见</li>
+						<li>{this.getMyCheckbox('visible')}</li>
+						<li>是否必输项</li>
+						<li>{this.getMyCheckbox('required')}</li>
+						<li>能否编辑</li>
+						<li>{this.getMyCheckbox('disabled')}</li>
+						<li>是否可用</li>
+						<li>{this.getMyCheckbox('isenable')}</li>
+						<li>必须启用</li>
+						<li>{this.getMyCheckbox('mustuse')}</li>
+						<li>是否自动检查</li>
+						<li>{this.getMyCheckbox('ischeck')}</li>
+						<li>是否使用函数</li>
+						<li>{this.getMyCheckbox('usefunc')}</li>
+						<li>精度</li>
+						<li>{this.getMyInput('精度', 'scale')}</li>
+						<li>单位</li>
+						<li>{this.getMyInput('单位', 'unit')}</li>
+						<li>计算比率</li>
+						<li>{this.getMyInput('计算比率', 'ratio')}</li>
+						<li>格式化类型</li>
+						<li>{this.getMyInput('格式化类型', 'formattype')}</li>
+						<li>操作符</li>
+						<li>{this.getMyInput('操作符', 'opersign')}</li>
+						<li>默认取值</li>
+						<li>{this.getMyInput('默认取值', 'defaultvalue')}</li>
+						<li>数据类型</li>
+						<li>{utilService.getDatatypeName(selectCard.datatype) }</li>
+					</ul>
+				</TabPane>
 				<TabPane tab='高级属性' key='2' />
 			</Tabs>
 		);
 	};
-	getDom2 = (datype) => {
+	getDom2 = () => {
 		return (
 			<Tabs defaultActiveKey='1'>
 				<TabPane tab='显示属性' key='1'>
