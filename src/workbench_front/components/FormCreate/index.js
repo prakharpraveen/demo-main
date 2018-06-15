ff01151f1802c203de688c5eed0d5722a7dfd5ef
gaokung <gaokung@yonyou.com>
2018-06-15 18:01:39 +0800

    update

--- src/workbench_front/components/FormCreate/index.js
+++ src/workbench_front/components/FormCreate/index.js
@@ -1,11 +1,11 @@
-import React, {Component} from "react";
-import {Form, Row, Col, Input, Select, Checkbox} from "antd";
-import {FormControl} from "./FormControl";
-import {high} from "nc-lightapp-front";
+import React, { Component } from "react";
+import { Form, Row, Col, Input, Select, Checkbox } from "antd";
+import { FormControl } from "./FormControl";
+import { high } from "nc-lightapp-front";
 import ChooseImageForForm from "Components/ChooseImageForForm";
 import "nc-lightapp-front/dist/platform/nc-lightapp-front/index.css";
 import "./index.less";
-const {Refer} = high;
+const { Refer } = high;
 const FormItem = Form.Item;
 const Option = Select.Option;
 /**
@@ -13,51 +13,51 @@ const Option = Select.Option;
  * 其中包括 type 类型为 string
  */
 class NormalShow extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return <div>{this.props.value}</div>;
-    }
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div>{this.props.value}</div>;
+  }
 }
 /**
  * 参照显示
  * type 类型为 refer
  */
 class ReferShow extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        // console.log(this.props);
-        return (
-            <div>
-                {this.props.value && this.props.value.refname
-                    ? this.props.value.refname
-                    : ""}
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    // console.log(this.props);
+    return (
+      <div>
+        {this.props.value && this.props.value.refname
+          ? this.props.value.refname
+          : ""}
+      </div>
+    );
+  }
 }
 /**
  * 下拉显示
  * type 类型为 select
  */
 class SelectShow extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+  }
+  showValue = () => {
+    let { options, value } = this.props;
+    let option = options.find(item => item.value === value);
+    if (option) {
+      return option.text;
     }
-    showValue = () => {
-        let {options, value} = this.props;
-        let option = options.find(item => item.value === value);
-        if (option) {
-            return option.text;
-        }
-        return "";
-    };
-    render() {
-        return <div>{this.showValue()}</div>;
-    }
+    return "";
+  };
+  render() {
+    return <div>{this.showValue()}</div>;
+  }
 }
 /**
  * 表单创建组件
@@ -75,249 +75,235 @@ class SelectShow extends Component {
  * onChange - 表单值改变事件
  */
 class FormContent extends Component {
-    constructor(props) {
-        super(props);
-    }
-    createComponent = item => {
-        const {getFieldDecorator} = this.props.form;
-        let {
-            type = "string",
-            isRequired = false,
-            label = "",
-            code,
-            isedit = false,
-            check
-        } = item;
-        switch (type) {
-            case "string":
-                return (
-                    <FormItem
-                        className="form-item margin-bottom-20"
-                        label={label}>
-                        {getFieldDecorator(code, {
-                            initialValue: "",
-                            rules: [
-                                {
-                                    type: "string",
-                                    message: "当前字段数据类型-string"
-                                },
-                                {
-                                    required: isRequired,
-                                    message: "此字段为必输项！"
-                                },
-                                {
-                                    validator: check ? check : null
-                                }
-                            ]
-                        })(isedit ? <Input /> : <NormalShow />)}
-                    </FormItem>
-                );
-            case "refer":
-                return (
-                    <FormItem
-                        className="form-item margin-bottom-20"
-                        label={label}>
-                        {getFieldDecorator(code, {
-                            initialValue: {},
-                            rules: [
-                                {
-                                    required: isRequired,
-                                    message: "此字段为必输项！"
-                                },
-                                {
-                                    type: "object",
-                                    message: "当前字段数据类型-object",
-                                    validator: null
-                                }
-                            ]
-                        })(
-                            isedit ? <Refer {...item.options} /> : <ReferShow />
-                        )}
-                    </FormItem>
-                );
-            case "select":
-                return (
-                    <FormItem
-                        className="form-item margin-bottom-20"
-                        label={label}>
-                        {getFieldDecorator(code, {
-                            initialValue: {},
-                            rules: [
-                                {
-                                    type: "string",
-                                    message: "当前字段数据类型-string",
-                                    validator: null
-                                },
-                                {
-                                    required: isRequired,
-                                    message: "此字段为必输项！"
-                                }
-                            ]
-                        })(
-                            isedit ? (
-                                <Select placeholder={`请选择${label}`}>
-                                    {this.createOption(item.options)}
-                                </Select>
-                            ) : (
-                                <SelectShow options={item.options} />
-                            )
-                        )}
-                    </FormItem>
-                );
-            case "checkbox":
-                return (
-                    <FormItem>
-                        {getFieldDecorator(code, {
-                            initialValue: false,
-                            valuePropName:'checked'
-                        })(<Checkbox disabled={!isedit}>{label}</Checkbox>)}
-                    </FormItem>
-                );
-            case "chooseImage":
-                return (
-                    <FormItem label={label}>
-                        {getFieldDecorator(code, {
-                            initialValue: "",
-                            rules: [
-                                {
-                                    required: isRequired,
-                                    message: "请选择一张图片！"
-                                }
-                            ]
-                        })(
-                            isedit ? (
-                                <ChooseImageForForm
-                                    data={item.options}
-                                    title={"图标选择"}
-                                />
-                            ) : (
-                                <ChooseImageForForm
-                                    data={item.options}
-                                    title={"已选"}
-                                />
-                            )
-                        )}
-                    </FormItem>
-                );
-            default:
-                break;
-        }
-    };
-    /**
-     * 创建表单项
-     */
-    createFormItem = () => {
-        let children = this.props.formData.map(item => {
-            let {xs = 24, md = 12, lg = 12, code, hidden = false} = item;
-            if (hidden === true) {
-                return null;
-            }
-            return (
-                <Col xs={xs} md={md} lg={lg} key={code}>
-                    {this.createComponent(item)}
-                </Col>
-            );
-        });
-        return children;
-    };
-    /**
-     * 创建 下拉内容
-     * @param {Array} options 下拉项数组
-     */
-    createOption = options => {
-        return options.map((item, index) => {
-            return (
-                <Option key={item.value} value={item.value}>
-                    {item.text}
-                </Option>
-            );
-        });
-    };
-    render() {
+  constructor(props) {
+    super(props);
+  }
+  createComponent = item => {
+    const { getFieldDecorator } = this.props.form;
+    let {
+      type = "string",
+      isRequired = false,
+      label = "",
+      code,
+      isedit = false,
+      check
+    } = item;
+    switch (type) {
+      case "string":
         return (
-            <Form layout="inline">
-                <Row>{this.createFormItem()}</Row>
-            </Form>
+          <FormItem className="form-item margin-bottom-20" label={label}>
+            {getFieldDecorator(code, {
+              initialValue: "",
+              rules: [
+                {
+                  type: "string",
+                  message: "当前字段数据类型-string"
+                },
+                {
+                  required: isRequired,
+                  message: "此字段为必输项！"
+                },
+                {
+                  validator: check ? check : null
+                }
+              ]
+            })(isedit ? <Input /> : <NormalShow />)}
+          </FormItem>
         );
+      case "refer":
+        return (
+          <FormItem className="form-item margin-bottom-20" label={label}>
+            {getFieldDecorator(code, {
+              initialValue: {},
+              rules: [
+                {
+                  required: isRequired,
+                  message: "此字段为必输项！"
+                },
+                {
+                  type: "object",
+                  message: "当前字段数据类型-object",
+                  validator: null
+                }
+              ]
+            })(isedit ? <Refer {...item.options} /> : <ReferShow />)}
+          </FormItem>
+        );
+      case "select":
+        return (
+          <FormItem className="form-item margin-bottom-20" label={label}>
+            {getFieldDecorator(code, {
+              initialValue: {},
+              rules: [
+                {
+                  type: "string",
+                  message: "当前字段数据类型-string",
+                  validator: null
+                },
+                {
+                  required: isRequired,
+                  message: "此字段为必输项！"
+                }
+              ]
+            })(
+              isedit ? (
+                <Select placeholder={`请选择${label}`}>
+                  {this.createOption(item.options)}
+                </Select>
+              ) : (
+                <SelectShow options={item.options} />
+              )
+            )}
+          </FormItem>
+        );
+      case "checkbox":
+        return (
+          <FormItem>
+            {getFieldDecorator(code, {
+              initialValue: false,
+              valuePropName: "checked"
+            })(<Checkbox disabled={!isedit}>{label}</Checkbox>)}
+          </FormItem>
+        );
+      case "chooseImage":
+        return (
+          <FormItem label={label}>
+            {getFieldDecorator(code, {
+              initialValue: "",
+              rules: [
+                {
+                  required: isRequired,
+                  message: "请选择一张图片！"
+                }
+              ]
+            })(
+              <ChooseImageForForm
+                isedit={isedit}
+                data={item.options}
+                title={isedit ? "图标选择" : "已选图标"}
+              />
+            )}
+          </FormItem>
+        );
+      default:
+        break;
     }
+  };
+  /**
+   * 创建表单项
+   */
+  createFormItem = () => {
+    let children = this.props.formData.map(item => {
+      let { xs = 24, md = 12, lg = 12, code, hidden = false } = item;
+      if (hidden === true) {
+        return null;
+      }
+      return (
+        <Col xs={xs} md={md} lg={lg} key={code}>
+          {this.createComponent(item)}
+        </Col>
+      );
+    });
+    return children;
+  };
+  /**
+   * 创建 下拉内容
+   * @param {Array} options 下拉项数组
+   */
+  createOption = options => {
+    return options.map((item, index) => {
+      return (
+        <Option key={item.value} value={item.value}>
+          {item.text}
+        </Option>
+      );
+    });
+  };
+  render() {
+    return (
+      <Form layout="inline">
+        <Row>{this.createFormItem()}</Row>
+      </Form>
+    );
+  }
 }
 // export default FormControl(FormCreate);
 const FormCom = FormControl(FormContent);
 export class FormCreate extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        let {onChange, formData, fields} = this.props;
-        return <FormCom onChange={onChange} formData={formData} {...fields} />;
-    }
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    let { onChange, formData, fields } = this.props;
+    return <FormCom onChange={onChange} formData={formData} {...fields} />;
+  }
 }
 /**
  * 数据转换
  * @param {*} object
  */
 export const dataTransfer = (object, defaultObj) => {
-    if (JSON.stringify(object) == "{}") {
-        console.error("dataTransfer 函数参数不能为空对象");
-        return;
+  if (JSON.stringify(object) == "{}") {
+    console.error("dataTransfer 函数参数不能为空对象");
+    return;
+  }
+  let obj = {};
+  if (defaultObj) {
+    for (const key in defaultObj) {
+      if (object.hasOwnProperty(key)) {
+        const element = object[key];
+        obj[key] = {};
+        obj[key]["value"] = element;
+      }
     }
-    let obj = {};
-    if (defaultObj) {
-        for (const key in defaultObj) {
-            if (object.hasOwnProperty(key)) {
-                const element = object[key];
-                obj[key] = {};
-                obj[key]["value"] = element;
-            }
-        }
-    } else {
-        for (const key in object) {
-            if (object.hasOwnProperty(key)) {
-                const element = object[key];
-                obj[key] = {};
-                obj[key]["value"] = element;
-            }
-        }
+  } else {
+    for (const key in object) {
+      if (object.hasOwnProperty(key)) {
+        const element = object[key];
+        obj[key] = {};
+        obj[key]["value"] = element;
+      }
     }
-    return obj;
+  }
+  return obj;
 };
 /**
  * 数据还原
  */
 export const dataRestore = object => {
-    if (JSON.stringify(object) == "{}") {
-        console.error("dataTransfer 函数参数不能为空对象");
-        return;
+  if (JSON.stringify(object) == "{}") {
+    console.error("dataTransfer 函数参数不能为空对象");
+    return;
+  }
+  let obj = {};
+  for (const key in object) {
+    if (object.hasOwnProperty(key)) {
+      const element = object[key];
+      obj[key] = element.value;
     }
-    let obj = {};
-    for (const key in object) {
-        if (object.hasOwnProperty(key)) {
-            const element = object[key];
-            obj[key] = element.value;
-        }
-    }
-    return obj;
+  }
+  return obj;
 };
 /**
  * 数据检查
  */
 export const dataCheck = object => {
-    if (JSON.stringify(object) == "{}") {
-        console.error("dataTransfer 函数参数不能为空对象");
-        return;
+  if (JSON.stringify(object) == "{}") {
+    console.error("dataTransfer 函数参数不能为空对象");
+    return;
+  }
+  let objArray = [];
+  for (const key in object) {
+    if (object.hasOwnProperty(key)) {
+      const element = object[key];
+      if (element.errors) {
+        objArray.push(element);
+      }
     }
-    let objArray = [];
-    for (const key in object) {
-        if (object.hasOwnProperty(key)) {
-            const element = object[key];
-            if (element.errors) {
-                objArray.push(element);
-            }
-        }
-    }
-    if (objArray.length > 0) {
-        return true;
-    } else {
-        return false;
-    }
+  }
+  if (objArray.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
