1a5eab9dd980820ad547127b27cb463304e52bf9
zhanglmg <zhanglmg@yonyou.com>
2018-06-20 09:58:55 +0800

    去掉template11的路由

--- src/workbench_front/routes.js
+++ src/workbench_front/routes.js
@@ -75,10 +75,6 @@ const  TemplateSettingUnit = Loadable({
     loader: () => import("Pages/TemplateSetting-unit"),
     loading: Loading
 });
-const  TemplateSetting11 = Loadable({
-    loader: () => import("Pages/TemplateSetting11"),
-    loading: Loading
-});
 const IndividuationRegister = Loadable({
     loader: () => import("Pages/IndividuationRegister"),
     loading: Loading
@@ -154,10 +150,6 @@ const routes = [
     {
         path:"/TemplateSetting-unit",
         component:TemplateSettingUnit
-    },
-    {
-        path:"/TemplateSetting11",
-        component:TemplateSetting11
     },
     {
         path: "/ir",
