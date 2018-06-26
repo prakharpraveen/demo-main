c06dd3549fc8317f2483c0e1b272d743a62cda88
gaokung <gaokung@yonyou.com>
2018-06-26 17:25:44 +0800

    update

--- src/workbench_front/pages/UserInfo/PhoneEdit.js
+++ src/workbench_front/pages/UserInfo/PhoneEdit.js
@@ -1,11 +1,46 @@
 import React, { Component } from "react";
-import { Form, Input, Select } from "antd";
+import { Form, Input, Select, Button, Row, Col } from "antd";
 const FormItem = Form.Item;
 const Option = Select.Option;
 class PhoneEdit extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      prompt: false,
+      num: 0
+    };
   }
+  /**
+   * 获取验证码
+   */
+  handleGetCaptcha = () => {
+    if (this.state.num > 0) {
+      return;
+    }
+    this.setState(
+      {
+        prompt: true,
+        num: 60
+      },
+      this.captchaCount
+    );
+  };
+  /**
+   * 验证码计数
+   */
+  captchaCount = () => {
+    let fun = () => {
+      let num = this.state.num;
+      if (num === 0) {
+        this.setState({ prompt: false });
+        clearInterval(timer);
+      } else {
+        num--;
+      }
+      this.setState({ num });
+    };
+    let timer = setInterval(fun, 1000);
+  };
   render() {
     let { layout, fieldDecorator: getFieldDecorator } = this.props;
     const prefixSelector = getFieldDecorator("prefix", {
@@ -37,6 +72,26 @@ class PhoneEdit extends Component {
               }
             ]
           })(<Input addonBefore={prefixSelector} style={{ width: "100%" }} />)}
+        </FormItem>
+        <FormItem
+          {...layout}
+          label="验证码"
+          extra={
+            this.state.prompt ? "验证码已发送至您的手机，请注意查收！" : ""
+          }
+        >
+          <Row gutter={8}>
+            <Col span={12}>
+              {getFieldDecorator("captcha", {
+                rules: [{ required: true, message: "请输入验证码!" }]
+              })(<Input />)}
+            </Col>
+            <Col span={12}>
+              <Button onClick={this.handleGetCaptcha}>
+                获取验证码{this.state.num > 0 ? `(${this.state.num})` : ""}
+              </Button>
+            </Col>
+          </Row>
         </FormItem>
       </Form>
     );
