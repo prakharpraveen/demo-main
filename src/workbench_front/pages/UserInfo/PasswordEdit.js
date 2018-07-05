6563cad8356b87000f77c9f24102bb59cebaa1cc
gaokung <gaokung@yonyou.com>
2018-07-05 15:35:05 +0800

    更新密码修改

--- src/workbench_front/pages/UserInfo/PasswordEdit.js
+++ src/workbench_front/pages/UserInfo/PasswordEdit.js
@@ -1,74 +1,127 @@
 import React, { Component } from "react";
 import { Form, Input } from "antd";
+import Ajax from "Pub/js/ajax";
 const FormItem = Form.Item;
 class PasswordEdit extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      confirmDirty: false
+    constructor(props) {
+        super(props);
+        this.state = {
+            validate: false,
+            confirmDirty: false
+        };
+    }
+    handlePwCheck = e => {
+        const value = e.target.value;
+        if (value === "") {
+            this.setState({ validate: false });
+            return;
+        }
+        Ajax({
+            url: `/nccloud/platform/appregister/checkuserpwd.do`,
+            info: {
+                name: "账户设置",
+                action: "密码校验"
+            },
+            data: {
+                oldPassword: value
+            },
+            success: ({ data: { data } }) => {
+                if (data.msg && data.msg.length > 0) {
+                    this.setState({ validate: true });
+                }
+            }
+        });
     };
-  }
-  handleConfirmBlur = e => {
-    const value = e.target.value;
-    this.setState({ confirmDirty: this.state.confirmDirty || !!value });
-  };
-  validateToNextPassword = (rule, value, callback) => {
-    const form = this.props.form;
-    if (value && this.state.confirmDirty) {
-      form.validateFields(["confirm"], { force: true });
+    handleConfirmBlur = e => {
+        const value = e.target.value;
+        this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+    };
+    validateToNextNewPassword = (rule, value, callback) => {
+        if (value === "") {
+            callback();
+        }
+        if (!this.state.validate) {
+            callback("密码不正确!");
+        }
+        const form = this.props.formObj;
+        if (value && this.state.confirmDirty) {
+            form.validateFields(["newpw"], { force: true });
+        }
+        callback();
+    };
+    validateToNextPassword = (rule, value, callback) => {
+        const form = this.props.formObj;
+        if (value && value === form.getFieldValue("pw")) {
+            callback("新密码不能与原始密码相同!");
+        }
+        if (value && this.state.confirmDirty) {
+            form.validateFields(["confirmpw"], { force: true });
+        }
+        callback();
+    };
+    compareToFirstPassword = (rule, value, callback) => {
+        const form = this.props.formObj;
+        if (value && value !== form.getFieldValue("newpw")) {
+            callback("您输入的两个密码不一致!");
+        } else {
+            callback();
+        }
+    };
+    render() {
+        let { layout, formObj } = this.props;
+        let { getFieldDecorator } = formObj;
+        return (
+            <Form>
+                <FormItem className="userinfo-item" {...layout} label="旧密码">
+                    {getFieldDecorator("pw", {
+                        rules: [
+                            {
+                                required: true,
+                                message: "请输入原始密码！"
+                            },
+                            {
+                                validator: this.validateToNextNewPassword
+                            }
+                        ]
+                    })(<Input onBlur={this.handlePwCheck} type="password" />)}
+                </FormItem>
+                <FormItem className="userinfo-item" {...layout} label="新密码">
+                    {getFieldDecorator("newpw", {
+                        rules: [
+                            {
+                                required: true,
+                                message: "请输入新密码！"
+                            },
+                            {
+                                validator: this.validateToNextPassword
+                            }
+                        ]
+                    })(<Input type="password" />)}
+                </FormItem>
+                <FormItem
+                    className="userinfo-item"
+                    {...layout}
+                    label="确认密码"
+                >
+                    {getFieldDecorator("confirmpw", {
+                        rules: [
+                            {
+                                required: true,
+                                message: "请再次确认新密码！"
+                            },
+                            {
+                                validator: this.compareToFirstPassword
+                            }
+                        ]
+                    })(
+                        <Input
+                            type="password"
+                            onBlur={this.handleConfirmBlur}
+                        />
+                    )}
+                </FormItem>
+            </Form>
+        );
     }
-    callback();
-  };
-  compareToFirstPassword = (rule, value, callback) => {
-    const form = this.props.form;
-    if (value && value !== form.getFieldValue("password")) {
-      callback("Two passwords that you enter is inconsistent!");
-    } else {
-      callback();
-    }
-  };
-  render() {
-    let { layout, fieldDecorator: getFieldDecorator } = this.props;
-    return (
-      <Form>
-        <FormItem className="userinfo-item" {...layout} label="旧密码">
-          {getFieldDecorator("pw", {
-            rules: [
-              {
-                required: true,
-                message: "请输入原始密码！"
-              }
-            ]
-          })(<Input type="password" />)}
-        </FormItem>
-        <FormItem className="userinfo-item" {...layout} label="新密码">
-          {getFieldDecorator("newpw", {
-            rules: [
-              {
-                required: true,
-                message: "请输入新密码！"
-              },
-              {
-                validator: this.validateToNextPassword
-              }
-            ]
-          })(<Input type="password" />)}
-        </FormItem>
-        <FormItem className="userinfo-item" {...layout} label="确认密码">
-          {getFieldDecorator("confirmpw", {
-            rules: [
-              {
-                required: true,
-                message: "请再次确认新密码！"
-              },
-              {
-                validator: this.compareToFirstPassword
-              }
-            ]
-          })(<Input type="password" onBlur={this.handleConfirmBlur} />)}
-        </FormItem>
-      </Form>
-    );
-  }
 }
 export default PasswordEdit;
--- src/workbench_front/pages/UserInfo/index.js
+++ src/workbench_front/pages/UserInfo/index.js
@@ -7,131 +7,131 @@ import PhoneEdit from "./PhoneEdit";
 import EmailEdit from "./EmailEdit";
 import "./index.less";
 class SwitchInfo extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    const formItemLayout = {
-      labelCol: {
-        xs: { span: 24 },
-        sm: { span: 8 }
-      },
-      wrapperCol: {
-        xs: { span: 24 },
-        sm: { span: 16 }
-      }
-    };
-    switch (this.props.infoType) {
-      // 密码修改
-      case "0":
-        return (
-          <PasswordEdit
-            layout={formItemLayout}
-            fieldDecorator={this.props.fieldDecorator}
-          />
-        );
-      // 手机修改
-      case "1":
-        return (
-          <PhoneEdit
-            layout={formItemLayout}
-            fieldDecorator={this.props.fieldDecorator}
-          />
-        );
-      // 邮箱修改
-      case "2":
-        return (
-          <EmailEdit
-            layout={formItemLayout}
-            fieldDecorator={this.props.fieldDecorator}
-          />
-        );
-      default:
-        return "";
+    constructor(props) {
+        super(props);
     }
-  }
+    render() {
+        const formItemLayout = {
+            labelCol: {
+                xs: { span: 24 },
+                sm: { span: 8 }
+            },
+            wrapperCol: {
+                xs: { span: 24 },
+                sm: { span: 16 }
+            }
+        };
+        switch (this.props.infoType) {
+            // 密码修改
+            case "0":
+                return (
+                    <PasswordEdit
+                        layout={formItemLayout}
+                        formObj={this.props.formObj}
+                    />
+                );
+            // 手机修改
+            case "1":
+                return (
+                    <PhoneEdit
+                        layout={formItemLayout}
+                        formObj={this.props.formObj}
+                    />
+                );
+            // 邮箱修改
+            case "2":
+                return (
+                    <EmailEdit
+                        layout={formItemLayout}
+                        formObj={this.props.formObj}
+                    />
+                );
+            default:
+                return "";
+        }
+    }
 }
 class UserInfo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      editData: {},
-      infoType: "",
-      modalTitle: "",
-      visible: false
-    };
-  }
-  /**
-   * 弹框标题选择
-   */
-  switchTitle = key => {
-    switch (key) {
-      case "0":
-        return "密码修改";
-      case "1":
-        return "手机修改";
-      case "2":
-        return "电子邮箱修改";
-      default:
-        break;
+    constructor(props) {
+        super(props);
+        this.state = {
+            editData: {},
+            infoType: "",
+            modalTitle: "",
+            visible: false
+        };
     }
-  };
-  showModal = infoType => {
-    let modalTitle = this.switchTitle(infoType);
-    this.setState({
-      visible: true,
-      infoType,
-      modalTitle
-    });
-  };
-  handleOk = e => {
-    console.log(e);
-    this.setState({
-      visible: false
-    });
-  };
-  handleCancel = e => {
-    console.log(e);
-    this.setState({
-      visible: false
-    });
-  };
-  // 表单提交
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.form.validateFieldsAndScroll((err, values) => {
-      if (!err) {
-        console.log("Received values of form: ", values);
-      }
-    });
-  };
-  render() {
-    const { getFieldDecorator } = this.props.form;
-    return (
-      <PageLayout header={<PageLayoutHeader>账户设置</PageLayoutHeader>}>
-        <div className="workbench-userinfo">
-          <InfoForm infoSetting={this.showModal} />
-          <Modal
-            title={this.state.modalTitle}
-            mask={false}
-            wrapClassName="vertical-center-modal"
-            visible={this.state.visible}
-            onOk={this.handleOk}
-            onCancel={this.handleCancel}
-            okText={"确认"}
-            cancelText={"取消"}
-          >
-            <div className="userinfo-modal-content">
-              <SwitchInfo
-                infoType={this.state.infoType}
-                fieldDecorator={getFieldDecorator}
-              />
-            </div>
-          </Modal>
-        </div>
-      </PageLayout>
-    );
-  }
+    /**
+     * 弹框标题选择
+     */
+    switchTitle = key => {
+        switch (key) {
+            case "0":
+                return "密码修改";
+            case "1":
+                return "手机修改";
+            case "2":
+                return "电子邮箱修改";
+            default:
+                break;
+        }
+    };
+    showModal = infoType => {
+        let modalTitle = this.switchTitle(infoType);
+        this.setState({
+            visible: true,
+            infoType,
+            modalTitle
+        });
+    };
+    handleOk = e => {
+        let allValue = this.props.form.getFieldsValue();
+        console.log(allValue);
+        this.setState({
+            visible: false
+        });
+    };
+    handleCancel = e => {
+        console.log(e);
+        this.setState({
+            visible: false
+        });
+    };
+    // 表单提交
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.form.validateFieldsAndScroll((err, values) => {
+            if (!err) {
+                console.log("Received values of form: ", values);
+            }
+        });
+    };
+    render() {
+        return (
+            <PageLayout header={<PageLayoutHeader>账户设置</PageLayoutHeader>}>
+                <div className="workbench-userinfo">
+                    <InfoForm infoSetting={this.showModal} />
+                    <Modal
+                        title={this.state.modalTitle}
+                        mask={false}
+                        wrapClassName="vertical-center-modal"
+                        visible={this.state.visible}
+                        onOk={this.handleOk}
+                        onCancel={this.handleCancel}
+                        okText={"确认"}
+                        cancelText={"取消"}
+                    >
+                        <div className="userinfo-modal-content">
+                            <SwitchInfo
+                                infoType={this.state.infoType}
+                                formObj={this.props.form}
+                            />
+                        </div>
+                    </Modal>
+                </div>
+            </PageLayout>
+        );
+    }
 }
 const WrappedUserInfo = Form.create()(UserInfo);
 export default WrappedUserInfo;
