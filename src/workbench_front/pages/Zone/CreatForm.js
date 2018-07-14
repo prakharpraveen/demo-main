28f2bf525a462d31c4d1c2306f0ee7dbe173e642
zhanglmg <zhanglmg@yonyou.com>
2018-07-14 18:20:53 +0800

    解决页面注册信息验证有误的问题

--- src/workbench_front/pages/Zone/CreatForm.js
+++ src/workbench_front/pages/Zone/CreatForm.js
@@ -1,6 +1,7 @@
 import { Col, Form, Input, Select } from 'antd';
 import React from 'react';
 const FormItem = Form.Item;
+import Notice from 'Components/Notice';
 /**
  * 创建表单
  * @param {*} propsData 
@@ -9,17 +10,20 @@ const FormItem = Form.Item;
  */
 export const createForm = (DOMDATA, propsData) => {
     const children = [];
-    DOMDATA && DOMDATA.map((item, index) => {
-        let { lable, md = 24, lg = 12, xl = 8 } = item;
-        children.push(
-            <Col md={md} lg={lg} xl={xl} key={index}>
-                {createFormItem(propsData, item)}
-            </Col>
-        );
-    });
+    DOMDATA &&
+        DOMDATA.map((item, index) => {
+            let { lable, md = 24, lg = 12, xl = 8 } = item;
+            children.push(
+                <Col md={md} lg={lg} xl={xl} key={index}>
+                    {createFormItem(propsData, item)}
+                </Col>
+            );
+        });
 
-    return children.filter((item) => { return item.props.children });
-}
+    return children.filter((item) => {
+        return item.props.children;
+    });
+};
 /**
  * 创建表单
  * @param {*} props 
@@ -30,7 +34,15 @@ const createFormItem = (props, itemInfo) => {
     const { getFieldDecorator } = props.form;
     let { lable, type, code, required, check, search } = itemInfo;
     switch (type) {
-        case "select":
+        case 'select': 
+            if(nodeData.areaList&&nodeData.areaList.length>0){
+                nodeData.areaList.map((item)=>{
+                    if((nodeData[code]===item.code)&&(item.areatype!=='1')){
+                        Notice({ status: 'error', msg: '数据有误' });
+                        return ;
+                    }
+                })
+            }
             return (
                 <FormItem label={lable}>
                     {getFieldDecorator(code, {
@@ -38,19 +50,13 @@ const createFormItem = (props, itemInfo) => {
                         rules: [
                             {
                                 required: required,
-                                message: "此字段为必输项或请补充此表单区编码！"
+                                message: '此字段为必输项或请补充此表单区编码！'
                             }
                         ]
-                    })(
-
-                        <Select>
-                             {itemInfo.options}
-                        </Select>
-
-                    )}
+                    })(<Select>{itemInfo.options}</Select>)}
                 </FormItem>
             );
-
+            break;
         default:
             return (
                 <FormItem label={lable}>
@@ -60,12 +66,14 @@ const createFormItem = (props, itemInfo) => {
                             {
                                 required: required,
                                 message: `请输入${lable}`
-                            }, {
+                            },
+                            {
                                 validator: check ? check : null
                             }
                         ]
                     })(<Input placeholder={`请输入${lable}`} />)}
                 </FormItem>
-            )
+            );
+            break;
     }
 };
