5e77d2da9e167a20999643a7a7ca062bdd6c9a34
zhanglmg <zhanglmg@yonyou.com>
2018-07-20 16:02:29 +0800

    区域复制功能

--- src/workbench_front/pages/Zone/ZoneTable.js
+++ src/workbench_front/pages/Zone/ZoneTable.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Table, Input, Icon, Button, Popconfirm, Select } from 'antd';
+import { Table, Input, Icon, Button, Popconfirm, Select, Modal } from 'antd';
 import _ from 'lodash';
 import { setNewList } from 'Store/Zone/action';
 import Ajax from 'Pub/js/ajax';
+import Notice from 'Components/Notice';
 import MdDefaultClassEntityRef from 'Components/Refers/mdDefaultClassEntityRef';
 import 'nc-lightapp-front/dist/platform/nc-lightapp-front/index.css';
 const Option = Select.Option;
@@ -111,7 +112,7 @@ class EditableSelect extends Component {
                             showSearch
                             optionFilterProp='children'
                             value={value}
-                            style={{ width:'80%' }}
+                            style={{ width: '80%' }}
                             onChange={(selected) => this.handleChange(selected)}
                             filterOption={(input, option) =>
                                 option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
@@ -178,7 +179,7 @@ class RelateSelect extends Component {
                                         showSearch
                                         optionFilterProp='children'
                                         value={value}
-                                        style={{ width:'80%' }}
+                                        style={{ width: '80%' }}
                                         onChange={(selected) => this.handleChange(selected)}
                                         filterOption={(input, option) =>
                                             option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
@@ -199,7 +200,7 @@ class RelateSelect extends Component {
                                         showSearch
                                         optionFilterProp='children'
                                         value={value}
-                                        style={{ width:'80%' }}
+                                        style={{ width: '80%' }}
                                         onChange={(selected) => this.handleChange(selected)}
                                         filterOption={(input, option) =>
                                             option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
@@ -213,7 +214,7 @@ class RelateSelect extends Component {
                     </div>
                 ) : (
                     <div className='my-editable-cell-text-wrapper'>
-                        <span  className='cell-show-value'>{(value && switchType(value)) || ' '}</span>
+                        <span className='cell-show-value'>{(value && switchType(value)) || ' '}</span>
 
                         <Icon type='edit' className='my-editable-cell-icon' onClick={this.edit} />
                     </div>
@@ -297,7 +298,20 @@ class ZoneTable extends Component {
         super(props);
         this.state = {
             dataSource: [],
-            count: null
+            count: null,
+            areaName: '', //区域名称
+            areaCode: '', //区域编码
+            areaPk: '', //区域主键
+            areatype: '0', //区域类型
+            position: 1, //
+            metaname: null, //关联元数据
+            areastatus: '', //区域状态
+            relationcode: null, //关联编码
+            visible: false,
+            oldData: {
+                areaCode: '',
+                areaName: ''
+            }
         };
         this.columns = [
             {
@@ -322,7 +336,7 @@ class ZoneTable extends Component {
             {
                 title: '区域类型',
                 dataIndex: 'areatype',
-                width: '15%',
+                width: '10%',
                 render: (text, record) => {
                     if (record.pk_area) {
                         return <span>{switchType(text)}</span>;
@@ -382,24 +396,32 @@ class ZoneTable extends Component {
             },
             {
                 title: '操作',
-                width: '5%',
+                width: '10%',
                 dataIndex: 'operation',
                 render: (text, record) => {
                     return this.state.dataSource.length ? (
-                        <Popconfirm
-                            title='确认是否删除?'
-                            okText='确定'
-                            cancelText='取消'
-                            onConfirm={() => this.onDelete(record.key)}
-                        >
-                            <a href='javascript:;'>删除</a>
-                        </Popconfirm>
+                        <div>
+                            <span>
+                                <Popconfirm
+                                    title='确认是否删除?'
+                                    okText='确定'
+                                    cancelText='取消'
+                                    onConfirm={() => this.onDelete(record.key)}
+                                >
+                                    <a href='javascript:;'>删除</a>
+                                </Popconfirm>
+                            </span>
+                            <span style={{ marginLeft: '15px' }}>
+                                <a className='margin-right-5' onClick={() => this.copyRow(record)}>
+                                    复制
+                                </a>
+                            </span>
+                        </div>
                     ) : null;
                 }
             }
         ];
     }
-
     // 组件更新
     componentWillReceiveProps(nextProps) {
         if (nextProps.zoneDatas.areaList) {
@@ -415,7 +437,24 @@ class ZoneTable extends Component {
             this.props.setNewList(nextProps.zoneDatas.areaList);
         }
     }
-
+    //复制行
+    copyRow(record) {
+        let { oldData } = this.state;
+        oldData.areaCode = record.code;
+        oldData.areaName = record.name;
+        console.log(record);
+        this.setState({
+            visible: true,
+            areaCode: record.code,
+            areaName: record.name,
+            areaPk: record.pk_area,
+            oldData,
+            areatype: record.areatype,
+            metaname: record.metaname,
+            areastatus: record.areastatus,
+            relationcode: record.relationcode
+        });
+    }
     // 闭包 只对具体的单元格修改
     onCellChange = (key, dataIndex) => {
         return (value) => {
@@ -435,7 +474,6 @@ class ZoneTable extends Component {
             }
         };
     };
-
     onDelete = (key) => {
         const dataSource = [ ...this.state.dataSource ];
         this.setState({ dataSource: dataSource.filter((item) => item.key !== key) }, () => {
@@ -467,8 +505,82 @@ class ZoneTable extends Component {
             }
         );
     };
+    //复制保存
+    handleOk = (e) => {
+        let {
+            areaCode,
+            areaName,
+            areaPk,
+            oldData,
+            count,
+            dataSource,
+            areatype,
+            metaname,
+            areastatus,
+            relationcode
+        } = this.state;
+        if (!areaCode) {
+            Notice({ status: 'warning', msg: '请输入区域编码' });
+            return;
+        }
+        if (!areaName) {
+            Notice({ status: 'warning', msg: '请输入区域名称' });
+            return;
+        }
+        if (areaCode === oldData.areaCode || areaName === oldData.areaName) {
+            Notice({ status: 'warning', msg: '编码或名称重复' });
+            return;
+        }
+        let infoData = {
+            areaid: areaPk,
+            code: areaCode,
+            name: areaName
+        };
+        Ajax({
+            url: `nccloud/platform/templet/copyarea.do`,
+            data: infoData,
+            info: {
+                name: '区域复制',
+                action: '区域复制'
+            },
+            success: ({ data }) => {
+                if (data.success) {
+                    Notice({ status: 'success', msg: '复制成功' });
+                    this.setState({
+                        visible: false
+                    });
+                    const newData = {
+                        key: count,
+                        name: areaName,
+                        templetid: '',
+                        code: areaCode,
+                        metaid: '',
+                        pk_area: data.data,
+                        areatype: areatype,
+                        metaname: metaname,
+                        areastatus: areastatus,
+                        relationcode: relationcode
+                    };
+                    this.setState(
+                        {
+                            dataSource: [ ...dataSource, newData ],
+                            count: count + 1
+                        },
+                        () => {
+                            this.props.setNewList(this.state.dataSource);
+                        }
+                    );
+                }
+            }
+        });
+    };
+    handleCancel = (e) => {
+        this.setState({
+            visible: false
+        });
+    };
     render() {
-        let { dataSource } = this.state;
+        let { dataSource, visible, areaCode, areaName, areaPk } = this.state;
         dataSource &&
             dataSource.map((v, i) => {
                 v.position = i + 1;
@@ -480,6 +592,47 @@ class ZoneTable extends Component {
                     新增
                 </Button>
                 <Table bordered dataSource={dataSource} columns={columns} pagination={false} />
+                <Modal
+                    title='表格区域复制'
+                    okText='保存'
+                    cancelText='取消'
+                    mask={false}
+                    wrapClassName='vertical-center-modal template-code-add'
+                    visible={visible}
+                    onOk={this.handleOk}
+                    onCancel={this.handleCancel}
+                >
+                    <div className='areaName-item'>
+                        <label htmlFor=''>区域名称：</label>
+                        <Input
+                            placeholder='区域名称：'
+                            value={areaName}
+                            onChange={(e) => {
+                                const areaName = e.target.value;
+                                this.setState({
+                                    areaName
+                                });
+                            }}
+                        />
+                    </div>
+                    <div className='areaCode-item'>
+                        <label htmlFor=''>区域编码：</label>
+                        <Input
+                            placeholder='区域名称：'
+                            value={areaCode}
+                            onChange={(e) => {
+                                const areaCode = e.target.value;
+                                this.setState({
+                                    areaCode
+                                });
+                            }}
+                        />
+                    </div>
+                    <div className='areaPk-item'>
+                        <label htmlFor=''>区域主键：</label>
+                        <span>{areaPk ? areaPk : ''}</span>
+                    </div>
+                </Modal>
             </div>
         );
     }
