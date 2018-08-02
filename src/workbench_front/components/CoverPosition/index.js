51645194520bb111adcf502abb9975c4f751cbbc
gaokung <gaokung@yonyou.com>
2018-08-02 17:09:20 +0800

    ie10优化

--- src/workbench_front/components/CoverPosition/index.js
+++ src/workbench_front/components/CoverPosition/index.js
@@ -1,14 +1,14 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import NoData from "Assets/images/nodata.png";
-import './index.less';
+import "./index.less";
 class CoverPosition extends Component {
     constructor(props) {
         super(props);
     }
     render() {
         return (
-            <div className='CoverPosition-container'>
-                <div className='CoverPosition-content'>
+            <div className="CoverPosition-container">
+                <div className="CoverPosition-content">
                     <img src={NoData} />
                     <span>暂无数据</span>
                 </div>
